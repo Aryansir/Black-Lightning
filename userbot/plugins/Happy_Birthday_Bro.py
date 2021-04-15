f0cfac9433af696093a9ecf4e290dfd4b87d605a
Rishisuperyo OP <73427470+Rishisuperyo-Op@users.noreply.github.com>
2021-04-15 10:58:30 +0530

    Create Happy_Birthday_Bro.py
    I am noob or pro ,it not depends,I test plugins now let's give bl fun😎⚡ Keinshin and developers an members just see what I do😎 wait and watch😎⚡🔥

---
+++ userbot/plugins/Happy_Birthday_Bro.py
@@ -0,0 +1,47 @@
+#Plugin by @Rishisuperyo
+#kang= gey, keep credits= cool coder😎⚡
+
+
+from telethon import events
+import asyncio
+@borg.on(events.NewMessage(pattern=r"^.hbtybro", outgoing=True))
+async def _(event):
+     a="🥳🎂🍰 🥳🎂🍰Happy Birthday🥳🥳🎂🍰"
+     await event.edit (a)
+     await asyncio.sleep(2)
+     b="Today is ur birthday,🥳I wish it goes in op style, here is a gift for u🎁"
+     await event.edit(b)
+     await asyncio.sleep(2)
+     c="lets invite ur all friends 🎁,make this birthday memorable"
+     await event.edit(c)
+     await asyncio.sleep(2)
+     d="friends have came here🥳,with many gifts🎁"
+     await event.edit(d)
+     await asyncio.sleep(2)
+     e="in this day start a new day in life, forgot all sadness and lets come in happy day🥳🎂🍰🎁"
+     await event.edit(e)
+     await asyncio.sleep(2)
+     f="here ur big, delicious cake🎂 is baking, and is gettig ready to eat"
+     await event.edit(f)
+     await asyncio.sleep(2)
+     g="`baking...0%\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `\n\n\n baking the cake"
+     await event.edit(g)
+     h="`baking...4%\n█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `\n\n\n giving cake a style tip"
+     await event.edit(h)
+     i="`baking...8%\n██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `\n\n\n giving the cake a beautiful shape🎁🎂"
+     await event.edit(i)
+     await asyncio.sleep(2)
+     j="`baking...20%\n█████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `\n\n\n pouring an delicious chocolate syrup on it🍫"
+     await event.edit(j)
+     await asyncio.sleep(2)  
+     k="`baking...36%\n█████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `\n\n\n mixing some vanila essence on it🍦"
+     await event.edit(k)
+     await asyncio.sleep(2)
+     l="`baking...52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ `\n\n\n placing it to another plate,and placing to microwave for 10 mins 🎂"
+     await event.edit(l)
+     await asyncio.sleep(2)
+     m="`baking...84%\n█████████████████████▒▒▒▒ `\n\n\n cake is looking delicious and is so much big, almost done🎂"
+     await event.edit(m)
+     await asyncio.sleep(2)
+     n="`baked...100%\n█████████B A K E D█████████ `\n\n\n the cake is fully baked,and is looking delicious and is ready to eat🎂"
+     await event.edit(n)
