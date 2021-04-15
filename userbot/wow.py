2187f0ca7794bd594584814bc945cc86c7c0de74
Rishisuperyo OP <73427470+Rishisuperyo-Op@users.noreply.github.com>
2021-04-15 10:19:37 +0530

    Create wow.py
    I am noob or pro ,it not depends,I test plugins now let's give bl fun😎⚡ Keinshin and developers an members just see what I do😎 wait and watch😎⚡🔥

---
+++ userbot/wow.py
@@ -0,0 +1,15 @@
+#Plugin By @Rishisuperyo
+#Kang = ultra gey
+#keep credits = cool coder😎
+#usage :-.wow
+#plz dont kang it take a lot of time to made😭
+#, plz keep credits of Rishisuperyo ⚡
+from telethon import events
+import asyncio
+from uniborg.util import lightning_cmd
+from userbot import CMD_HELP, ALIVE_NAME
+@borg.on(events.NewMessage(pattern=r"^.wow", outgoing=True))
+async def hapy(event):
+
+     a="░█──░█ ░█▀▀▀█ ░█──░█ \n░█░█░█ ░█──░█ ░█░█░█ \n░█▄▀▄█ ░█▄▄▄█ ░█▄▀▄█"
+     await event.edit(a)
