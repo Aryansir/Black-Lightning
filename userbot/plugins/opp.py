069255192257418723b6ffaa866d44a760c0e6ea
Rishisuperyo OP <73427470+Rishisuperyo-Op@users.noreply.github.com>
2021-04-16 14:46:25 +0530

    Update opp.py
    I am noob or pro ,it not depends,I test plugins now let's give bl fun😎⚡ Keinshin and developers an members just see what I do😎 wait and watch😎⚡🔥

--- userbot/plugins/opp.py
+++ userbot/plugins/opp.py
@@ -9,7 +9,7 @@ import asyncio
 from userbot.utils import lightning_cmd
 
 from userbot import CMD_HELP
-@borg.on(lightning_cmd(pattern=r".opp"))
+@borg.on(lightning_cmd(pattern=r"opp"))
 async def hapy(event):
      a="░█▀▀▀█     ░█▀▀▀█\n░█──░█     ░█──░█\n░█──░█     ░█▀▀▀▀\n░█──░█     ░█\n░█▄▄▄█     ░█"
      await event.edit(a)
