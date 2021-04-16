5de48790776f0a3f8e370e8add5c358f3194dc82
Rishisuperyo OP <73427470+Rishisuperyo-Op@users.noreply.github.com>
2021-04-16 10:58:18 +0530

    Create thunder.py
    I am noob or pro ,it not depends,I test plugins now let's give bl fun😎⚡ Keinshin and developers an members just see what I do😎 wait and watch😎⚡🔥

---
+++ userbot/plugins/thunder.py
@@ -0,0 +1,13 @@
+#Plugin by @Rishisuperyo
+#kang =gey ,keep credits = cool coder 😶😎⚡
+#usage .thunder
+from telethon import events
+import asyncio
+from userbot.utils import lightning_cmd
+
+from userbot import CMD_HELP
+@borg.on(lightning_cmd(pattern=r".thunder"))
+async def hapy(event):
+	
+     a="ㅤㅤ  █\n         █\n        █\n       █\n      █░░░░░░█\n                        █\n                        █\n                       █\n                     █\n\n`ALL IS WORKING ⚡ MY OP ⚡MASTER⚡ 😎⚡`"
+     await event.edit(a)
