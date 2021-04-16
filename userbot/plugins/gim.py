56f4f83555726ef1156434b5aad9683bed543e17
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-04-16 12:21:54 +0530

    Saare noobs he bc

--- userbot/plugins/gim.py
+++ userbot/plugins/gim.py
@@ -8,7 +8,7 @@ import asyncio
 from userbot.utils import lightning_cmd
 
 from userbot import CMD_HELP
-@borg.on(lightning_cmd(pattern=r^".gim"))
+@borg.on(lightning_cmd(pattern="gim"))
 async def hapy(event):
 
      a="🎱➖✊➖➖✊➖🎱\n🌟        \         /          🌟\n⭐          \😁/            ⭐\n✨           🎽             ✨\n              /    \ \n            👟    👟"
