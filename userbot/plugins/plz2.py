317ed374e55fe0d081300cc5440fe941be652be0
Rishisuperyo OP <73427470+Rishisuperyo-Op@users.noreply.github.com>
2021-04-20 17:13:40 +0530

    Galti sa mistake in Hindi Chahiye ⚡,line Mat hatana Friends
    I am noob or pro ,it not depends,I test plugins now let's give bl fun😎⚡ Keinshin and developers an members just see what I do😎 wait and watch😎⚡🔥

--- userbot/plugins/plz2.py
+++ userbot/plugins/plz2.py
@@ -6,7 +6,7 @@ from telethon import events
 import asyncio
 from userbot.utils import lightning_cmd
 from userbot import CMD_HELP
-@borg.on(lightning_cmd(pattern=r"^plzzz", outgoing=True))
+@borg.on(lightning_cmd(pattern=r"plzzz", outgoing=True))
 async def _(event):
      a="plz 🥺🙏🥺🙏🥺🙏🥺🙏🥺🙏🥺"
      await event.edit(a)
