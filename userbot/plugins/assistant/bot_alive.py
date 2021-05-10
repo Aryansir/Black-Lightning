894046e3e9fa23b28da0c94e0e2ee2a02daa51e3
Krishna <72682463+krishna6688@users.noreply.github.com>
2021-05-10 15:07:40 +0530

    Update bot_alive.py

--- userbot/plugins/assistant/bot_alive.py
+++ userbot/plugins/assistant/bot_alive.py
@@ -10,17 +10,12 @@ PM_IMG = "https://telegra.ph/file/7f72b0ea1893e84028298.mp4"
 pm_caption = "➥ **ASSISTANT IS:** `ONLINE`\n\n"
 pm_caption += "➥ **SYSTEMS STATS**\n"
 pm_caption += "➥ **Telethon Version:** `1.15.0` \n"
-pm_caption += "➥ **Python:** `3.7.4` \n"
-pm_caption += "➥ **Database Status:**  `Functional`\n"
-pm_caption += "➥ **Current Branch** : `master`\n"
 pm_caption += f"➥ **Version** : `{currentversion}`\n"
-pm_caption += f"➥ **My Boss** : {DEFAULTUSER} \n"
-pm_caption += "➥ **Heroku Database** : `AWS - Working Properly`\n\n"
+pm_caption += f"➥ **My Master** : {DEFAULTUSER} \n"
 pm_caption += "➥ **License** : [GNU General Public License v3.0](https://github.com/KeinShin/Black-Lightning/blob/master/LICENSE)\n"
-pm_caption += "➥ **Copyright** : By [@krih1303y](GitHub.com/Kenshin)\n"
-pm_caption += "[Assistant By Black Lightning 🇮🇳](https://telegra.ph/file/b233f8b6332fbeb3f61dc.mp4)"
-
+pm_caption += "➥ **Copyright** : [Raiden-devs](GitHub.com/Raiden-Devs)\n"
+BUTTON = [[Button.url("Repos", f"https://github.com/KeinShin/Black-Lightning"), Button.url(f"Support", "https://t.me/lightning_support_group")]]
 
 @tgbot.on(events.NewMessage(pattern="^/alive", func=lambda e: e.sender_id == bot.uid))
 async def _(event):
-    await tgbot.send_file(event.chat_id, PM_IMG, caption=pm_caption)
+    await tgbot.send_file(event.chat_id, PM_IMG, buttons=BUTTON, caption=pm_caption)
