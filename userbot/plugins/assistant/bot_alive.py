8050563bc3f35b1c36804adaff846dfdb942d8ed
Krishna <72682463+krishna6688@users.noreply.github.com>
2021-05-10 15:29:12 +0530

    Update bot_alive.py

--- userbot/plugins/assistant/bot_alive.py
+++ userbot/plugins/assistant/bot_alive.py
@@ -14,8 +14,8 @@ pm_caption += f"➥ **Version** : `{currentversion}`\n"
 pm_caption += f"➥ **My Master** : {DEFAULTUSER} \n"
 pm_caption += "➥ **License** : [GNU General Public License v3.0](https://github.com/KeinShin/Black-Lightning/blob/master/LICENSE)\n"
 pm_caption += "➥ **Copyright** : [Raiden-devs](GitHub.com/Raiden-Devs)\n"
-BUTTON = [[Button.url("Repos", f"https://github.com/KeinShin/Black-Lightning"), Button.url(f"Support", "https://t.me/lightning_support_group")]]
+BUTTON = [[Button.url("Repos", "https://github.com/KeinShin/Black-Lightning"), Button.url("Support", "https://t.me/lightning_support_group")]]
 
 @tgbot.on(events.NewMessage(pattern="^/alive", func=lambda e: e.sender_id == bot.uid))
 async def _(event):
-    await tgbot.send_file(event.chat_id, PM_IMG, buttons=BUTTON, caption=pm_caption)
+    await tgbot.send_file(event.chat_id, PM_IMG, caption=pm_caption, buttons=BUTTON)
