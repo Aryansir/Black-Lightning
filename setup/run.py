61b18c791e85ba00572213ea9230da4190655a4d
Kei <73867910+KeinShin@users.noreply.github.com>
2021-05-14 14:13:47 +0530

    Delete setup directory

--- setup/run.py
+++
@@ -1,3 +0,0 @@
-#To solbe suspension issue #By sh1vam
-import os
-os.system("git clone https://github.com/KeinShin/Black-Lightning -b rebirthed /root/system && mkdir /root/system/bin/ && cd /root/system/ && chmod +x /usr/local/bin/* && python3 -m system")
