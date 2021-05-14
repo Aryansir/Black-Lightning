ede52912b313e55e01f03c3c6b5985af71362847
Kei <73867910+KeinShin@users.noreply.github.com>
2021-05-14 14:38:07 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 #To solbe suspension issue #By sh1vam
 
 import os
-os.system("git clone https://github.com/KeinShin/Black-Lightning -b rebirthed /root/system && mkdir /root/system/bin/ && cd /root/system/ && chmod +x /usr/local/bin/* && python3 -m system")
+os.system("git clone https://github.com/KeinShin/Black-Lightning -b rebirth /root/system && mkdir /root/system/bin/ && cd /root/system/ && chmod +x /usr/local/bin/* && python3 -m system")
