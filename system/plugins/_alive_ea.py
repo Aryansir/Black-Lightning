e02a8edf1831ad756d09a35f46bf53e499d3599d
Kei <73867910+KeinShin@users.noreply.github.com>
2021-05-15 20:27:51 +0530

    Update _alive_ea.py

--- system/plugins/_alive_ea.py
+++ system/plugins/_alive_ea.py
@@ -3,12 +3,12 @@
 
 
 #  Hello sur 
-
+import time
 
 from system.decorators import on_cmd
 from system.Config import Variable
 from system.Config.utils import  get_readable_time
-ping = get_readable_time((time.time() - last_up))
+ping = get_readable_time((time.time() - time.time()))
 from system import *              # Easter OwO
 @on_cmd(["alive", "black", "alv", f"{OWNER}"], sudo=True, sudo_id=Variable.SUDO_IDS)
 async def alive(client, message):
