5f49de9e604fd88f2ffc71504eeb7d73b95c454a
Kei <73867910+KeinShin@users.noreply.github.com>
2021-05-15 20:22:16 +0530

    Update owner_message.py

--- system/user_bot_assistant/owner_message.py
+++ system/user_bot_assistant/owner_message.py
@@ -1,20 +1,20 @@
-from system.Config.utils import Owner
-from system import app, bot
-from pyrogram import filters
+# from system.Config.utils import Owner
+# from system import app, bot
+# from pyrogram import filters
 
-from system.Config import Variable
-# owner= filters.create(func=p.owner, name="Owmer")
-
-
+# from system.Config import Variable
+# # owner= filters.create(func=p.owner, name="Owmer")
 
 
-@bot.on_message(
-    filters.private
-    & filters.incoming
-    & owner
-    & ~filters.edited
-)
-async def owner(client, message):
-    pass
+
+
+# @bot.on_message(
+#     filters.private
+#     & filters.incoming
+#     & owner
+#     & ~filters.edited
+# )
+# async def owner(client, message):
+#     pass
     
 
