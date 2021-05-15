e3f2ce49ea11460b5586a9cd6499f7bd66e5e4b6
Kei <73867910+KeinShin@users.noreply.github.com>
2021-05-15 12:57:27 +0530

    Create ub-setup.sh

---
+++ ub-setup.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Copyright (C) 2021 KeinShin@Github.
+
+if [ -e exconfig.py  ]
+then
+    a="""
+LOADING USERBOT  - KINDLY WAIT 5min. 
+
+INITIALIZING - Self Hosting Setup 
+
+© Black-Lightning 2021"""
+    
+
+
+else
+    a =     """LOADING USERBOT  - KINDLY WAIT 5min. 
+
+USERBOT -  Black-Lightning
+
+© Black-Lightning 2021"""
+
+
+fi
+
+
+echo a
+
+
+
+
+
+echo "Installing Requirements"
+
+
+
+
+
+
+
+
+echo "INITIALIZING System"
+python3 -m system
