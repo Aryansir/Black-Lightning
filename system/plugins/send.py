d0e7cdbe16a255dba22e111c8bee290532e3d06c
Kei <73867910+KeinShin@users.noreply.github.com>
2021-05-16 22:30:05 +0530

    Update send.py

--- system/plugins/send.py
+++ system/plugins/send.py
@@ -13,13 +13,13 @@ async def send(client, message):
     txt=message.text.split()[1]
 
     if  Path(f'{txt}.py').is_file():
-        path = f"./zeda/plugins/{txt}.py"
+        path = f"./system/plugins/{txt}.py"
         await app.send_document(message.chat_id, document=path, caption=f"**PLUGINS** - __{txt}__\n\n@lightning_support_grup", force_document=True)
     elif "sql" in message.text:
         if not Path(f'{txt}.py').is_file():
             await message.edit_message_text("**SQL DOES NOT EXSISTS!**")
         else:
-           path = f"./zeda/sqls/{txt}.py"
+           path = f"./system/sqls/{txt}.py"
            await app.send_document(message.chat_id, document=path, caption=f"**SQL-FILE** - __{txt}__\n\n@lightning_support_grup", force_document=True)
     elif "requirements" in message.text:
         path = f"{txt}.txt"
