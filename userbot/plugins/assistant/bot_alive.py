5a8e5a3387c1a7425398f6ab9a72b0ec442cca7d
Krishna <72682463+krishna6688@users.noreply.github.com>
2021-05-18 16:20:47 +0530

    Update bot_alive.py

--- userbot/plugins/assistant/bot_alive.py
+++ userbot/plugins/assistant/bot_alive.py
@@ -2,20 +2,20 @@ from telethon import events, Button, custom
 
 from userbot import ALIVE_NAME, bot 
 
-currentversion = "4.9"
+currentversion = "2.1"
 
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Black Lightning"
-PM_IMG = "https://telegra.ph/file/7f72b0ea1893e84028298.mp4"
-pm_caption = "➥ **ASSISTANT IS:** `ONLINE`\n\n"
-pm_caption += "➥ **SYSTEMS STATS**\n"
-pm_caption += "➥ **Telethon Version:** `1.15.0` \n"
-pm_caption += f"➥ **Version** : `{currentversion}`\n"
-pm_caption += f"➥ **My Master** : {DEFAULTUSER} \n"
-pm_caption += "➥ **License** : [GNU General Public License v3.0](https://github.com/KeinShin/Black-Lightning/blob/master/LICENSE)\n"
-pm_caption += "➥ **Copyright** : [Raiden-devs](GitHub.com/Raiden-Devs)\n"
-light = [[Button.url("Repos", "https://github.com/KeinShin/Black-Lightning"), Button.url("Support", "https://t.me/lightning_support_group")]]
-light +=[[custom.Button.inline("Help", data="gibcmd")]]
+PM_IMG = "https://telegra.ph/file/beb843ce962a738c52cad.jpg"
+pm_caption = "➤ **ASSISTANT IS:** `ONLINE`\n\n"
+pm_caption += "➤ **SYSTEMS STATS**\n"
+pm_caption += "➤ **Telethon Version:** `1.15.0` \n"
+pm_caption += f"➤ **Assistant Version** : `{currentversion}`\n"
+pm_caption += f"➤ **My Master** : {DEFAULTUSER} \n"
+pm_caption += "➤ **License** : [GNU General Public License v3.0](https://github.com/KeinShin/Black-Lightning/blob/master/LICENSE)\n"
+pm_caption += "➤ **Copyright** : [Black Lighting](GitHub.com/keinshin/Black-Lightning)\n"
+light = [[Button.url("✧Repos✧", "https://github.com/KeinShin/Black-Lightning"), Button.url("✧Support✧", "https://t.me/lightning_support_group")]]
+light +=[[custom.Button.inline("✧Help✧", data="gibcmd")]]
 @tgbot.on(events.NewMessage(pattern="^/alive", func=lambda e: e.sender_id == bot.uid))
 async def _(event):
     await tgbot.send_file(event.chat_id, PM_IMG, caption=pm_caption, buttons=light)
