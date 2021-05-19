33715d249738c878363ae1848d8ae8113ee13197
Kei <73867910+KeinShin@users.noreply.github.com>
2021-05-19 19:17:09 +0530

    Update dictionary.py

--- system/plugins/dictionary.py
+++ system/plugins/dictionary.py
@@ -1,16 +1,20 @@
 from system.Config.utils import Variable
 from PyDictionary import PyDictionary
-
-from system.plugins import light
+from pyrogram import filters
+from system import light
 from system import (
     app,
     COMMAND_HELP,
     HNDLR
 )
 
-mean = ""
+import logging
 
-@light.on(["dict", "define", "meaning"], sudo_ids=Variable.SUDO_IDS)
+logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.ERROR)
+
+mean = ""
+@light.on(["dic", "define", "dictonary"])
 async def dc(client, message):
     if  not message.text:
         await message.edit_message_text("**How could i define nothing?**")
@@ -18,7 +22,7 @@ async def dc(client, message):
     txt = message.text.split()[1]
     meana = PyDictionary()
     meaning = meana.meaning(txt)
-    for i in meaning:
+    for i in meaning['Noun']:
         mean += "\n" + i
     await app.send_message(message.chat_id, f"__Meaning__{mean}")
    
@@ -26,3 +30,5 @@ COMMAND_HELP.update({
     "dictionary": f"`{HNDLR}dict or define or meaning`",
     "dictionary's help": "**Gets the meanign of that word from dictonary**"
 })
+
+
