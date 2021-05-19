cc284d7377963e78fc5700b5cee63352febd9f32
Kei <73867910+KeinShin@users.noreply.github.com>
2021-05-19 19:38:45 +0530

    Update dictionary.py

--- system/plugins/dictionary.py
+++ system/plugins/dictionary.py
@@ -1,7 +1,7 @@
 from system.Config.utils import Variable
 from PyDictionary import PyDictionary
 from pyrogram import filters
-from system.plugins import light
+from system import light
 from system import (
     app,
     COMMAND_HELP,
@@ -21,10 +21,12 @@ async def dc(client, message):
         return
     txt = message.text.split()[1]
     meana = PyDictionary()
+    no = 1
     meaning = meana.meaning(txt)
     for i in meaning['Noun']:
-        mean += "\n" + i
-    await app.send_message(message.chat_id, f"__Meaning__{mean}")
+        mean +=  "\n" + f'{no} - ' +  i
+        no += 1
+    await app.send_message(message.chat_id, f"__Meaning__\n\n**{mean}**")
    
 COMMAND_HELP.update({
     "dictionary": f"`{HNDLR}dict or define or meaning`",
