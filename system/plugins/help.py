28f9b64ba6e69650591bb7b3db65ec6ddad37141
Kei <73867910+KeinShin@users.noreply.github.com>
2021-05-19 18:54:22 +0530

    Update help.py

--- system/plugins/help.py
+++ system/plugins/help.py
@@ -10,16 +10,12 @@
 # import sys
 # import os
 # from sys import path
-# dir_path = "/absolute/path/to/E:\on_cmd-UserBot"
-# sys.path.insert(0, dir_path)
-# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from nltk.util import Index
 from pyrogram.methods import messages
 
 
 from system.Config import Variable
-from system.decorators import ERRORS_NAME
-from system.plugins import light
-
+from system.decorators import ERRORS_NAME, light
 from system import CMD_LIST, COMMAND_HELP, bot, app, SUDO_USER_NO_OF_TIME_USED
 from pyrogram.errors import BotInlineDisabled
 import logging
@@ -30,24 +26,27 @@ HNDLR = str(Variable.HNDLR)
 g =  Variable.TG_BOT_USER_NAME
 string = ""
 # unofficial_or_no_help = 0
-@light.on(["help"], sudo_ids=Variable.SUDO_IDS)
+@light().on(["help"])
 async def helper(client, message):
         count = 0
+        try:
 
- 
-        if " " in await message.text:
-          text = await  message.text.split()[1]
+         txt = message.text
+        except IndexError as e:
+          await message.edit(e)
+        if " " in  txt:
+          text = message.text.split()[1]
           try:
              for i in CMD_LIST:
                  string += HNDLR +  i + "\n"
                  string += "\n"
                  count += 1
-             await message.edit_message_text(f"🐱‍🚀** {count} Commands in {text}**🐱‍🚀\n\n`{i}`")
+             await message.edit(f"🐱‍🚀** {count} Commands in {text}**🐱‍🚀\n\n`{i}`")
           except KeyError:
-            await message.edit_message_text(f"**ERROR! No MODULE with name {text} either it is not available**")
+            await message.edit(f"**ERROR! No MODULE with name {text} either it is not available**")
             return
           except BaseException as e:
-            await message.edit_message_text(f"ERROR!: {e}")
+            await message.edit(f"ERROR!: {e}")
         else:
            try:
                 if not g.startswith("@"):
@@ -61,65 +60,9 @@ async def helper(client, message):
                bot_results.results[0].id
            )
            except BotInlineDisabled:
-             await message.edit_message_text(f"**Bot {g}  inline is disabled turn it on!**")
+             await message.edit(f"**Bot {g}  inline is disabled turn it on!**")
              return
            except BaseException as a:
              logging.error(a)
-             await  message.edit_message_text(f"**ERROR** - `{a}`\n\n**Occured while  opening help menu try doing** __@{g} Help Menu__\n\n**if help not appears contact @lightning_support_grup**")
+             await  message.edit(f"**ERROR** - `{a}`\n\n**Occured while  opening help menu try doing** __@{g} Help Menu__\n\n**if help not appears contact @lightning_support_grup**")
     
-
-@light.on(["errorcmds"],sudo_ids=Variable.SUDO_IDS)
-async def error(client, message):    
-  error_cmd = ""
-  cou = 0
-  for  i in ERRORS_NAME:
-      error_cmd += i
-      error_cmd += "\n"
-      cou += 1
-  await message.edit_message_text(f"**Total commands which was having error few times ago or currently are {cou}**\n\n`{error_cmd}`")
-
-
-
-# a = pd.DataFrame(SUDO_USER_NO_OF_TIME_USED)
-
-
-# @light.on(['sudo'])
-# async def used(client, message):
-
-
-#   horny_man = await message.text
-#   if  await message.text is None:
-#     await message.edit_message_text("**Gib some input :/**")
-#     return
-#   if " " in await  message.text:
-#     messagec = await  message.text.replace("sudo", "")
-#     text = messagec.split(",")
-#     name=text[1] 
-#     # text =  message.text.split()[1
-#     used = ""
-#     # a = pd.DataFrame(SUDO_USER_NO_OF_TIME_USED)
-#     s=a.loc[:, ['{}'.format(name), 'Time']]
-#     k=s.to_string(index=False)
-#     b = a[text]
-#     await app.send_message(message.chat_id, "**Number ids who have used or using** __{}__ **are**\n\n`{}`".format(name, k))
-#   elif "," in horny_man:
-#     messagec = await message.text.replace(f"{HNDLR}sudo", "")
-#     text = await messagec.split(",")
-#     name=text[0] 
-#     id = text[1]
-#     module_name=text[2]
-#     user =  await app.get_users(int(id))
-#     sed_user = a.loc["{}".format(module_name) == id]
-#     await app.send_message(message.chat_id, f"SUDO USER {user.first_name}]({id})\n{sed_user}")
-#   else:
-#     await message.edit_message_text(f"Gib some module name and user id  to check :/\n\n**Syntax: __{HNDLR}sudo_cmds id,name ( dont forget that ',' )")
-
-
-
-
-COMMAND_HELP.update({
-  "help": f"`{HNDLR}help` (command) or {HNDLR}help\
-  \n\n`{HNDLR}sudo` (username)",
-  "help's help": f"**USE**: {HNDLR}help (command) gets the detailed help without triggering help menu and {HNDLR}help triggers help menu\
-  \n\n`{HNDLR}sudo (username)` **will get the names who've used sudo plugins whom which you have given access."
-})
