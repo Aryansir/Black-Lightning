3d227cf0f7c97f6ea1f3d0e23377b9add96dc250
Kei <73867910+KeinShin@users.noreply.github.com>
2021-05-19 18:55:56 +0530

    Update send.py

--- system/plugins/send.py
+++ system/plugins/send.py
@@ -1,31 +1,35 @@
 # Copyright (C) 2021 KeinShin@Github.
 
 from system.Config.utils import Variable
-from system.plugins import light
+from system import light
 
 
 from system import *
-@light.on(["send"], sudo_ids=Variable.SUDO_IDS)
+@light.on(["send"])
 async def send(client, message):
+    try:
+        txt=message.text
+    except IndexError:
+        await message.edit(f"**Syntax**: __{HNDLR}send (file name)__\n\n@lightning_support_grup")
     if not " " in message.text:
-        await message.edit_message_text(f"**Syntax**: __{HNDLR}send (file name)__\n\n@lightning_support_grup")
+        await message.edit(f"**Syntax**: __{HNDLR}send (file name)__\n\n@lightning_support_grup")
         return
     txt=message.text.split()[1]
 
     if  Path(f'{txt}.py').is_file():
         path = f"./system/plugins/{txt}.py"
-        await app.send_document(message.chat_id, document=path, caption=f"**PLUGINS** - __{txt}__\n\n@lightning_support_grup", force_document=True)
+        await app.send_document(message.chat.id, document=path, caption=f"**PLUGINS** - __{txt}__\n\n@lightning_support_grup", force_document=True)
     elif "sql" in message.text:
         if not Path(f'{txt}.py').is_file():
-            await message.edit_message_text("**SQL DOES NOT EXSISTS!**")
+            await message.edit("**SQL DOES NOT EXSISTS!**")
         else:
            path = f"./system/sqls/{txt}.py"
-           await app.send_document(message.chat_id, document=path, caption=f"**SQL-FILE** - __{txt}__\n\n@lightning_support_grup", force_document=True)
+           await app.send_document(message.chat.id, document=path, caption=f"**SQL-FILE** - __{txt}__\n\n@lightning_support_grup", force_document=True)
     elif "requirements" in message.text:
         path = f"{txt}.txt"
-        await app.send_document(message.chat_id, document=path, caption=f"**REQUIREMENTS-FILE** - __{txt}__\n\n@lightning_support_grup", force_document=True)
+        await app.send_document(message.chat.id, document=path, caption=f"**REQUIREMENTS-FILE** - __{txt}__\n\n@lightning_support_grup", force_document=True)
     else:
-        await message.edit_message_text("{} does not exsists! (make sure name is correct :/)".format(txt))
+        await message.edit("{} does not exsists! (make sure name is correct :/)".format(txt))
     await message.delete()
 
 
