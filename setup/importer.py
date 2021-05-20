68a7dafc2d548f703611e2b02dc1e1245dc6ea3f
Kei <73867910+KeinShin@users.noreply.github.com>
2021-05-20 11:56:00 +0530

    Update importer.py

--- setup/importer.py
+++ setup/importer.py
@@ -4,7 +4,7 @@
 import logging
 import os
 import importlib
-from system import app
+import pyrogram
 
 
 class Start:
@@ -12,12 +12,11 @@ class Start:
         self.name = name
         path = self.name.split("/")
         self.path = ".".join(path)
-        
-        self.x= [x for x in os.listdir(self.name) if x.endswith(".py") and not x.startswith("__")]    
+        self.pat  = None
+        self.x= [x for x in os.listdir(self.name) if x.endswith(".py") and not x.startswith("__") ]    
     
 
     def boot(self):
-          for i in self.x:
-              a=f"{self.path}{i.replace('.py', '')}"
-              importlib.import_module(a)
-              logging.info(f"IMPORTED - {i}")
+          
+        a=f"{self.path}"
+        return    importlib.import_module(f"{a}{self.pat}")
