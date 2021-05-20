5892ae0dfedbecf691cba7505b8db461c2534b6a
Kei <73867910+KeinShin@users.noreply.github.com>
2021-05-20 12:53:39 +0530

    Update send.py

--- system/plugins/send.py
+++ system/plugins/send.py
@@ -16,21 +16,24 @@ async def send(client, message):
         return
     txt=message.text.split()[1]
 
-    if  Path(f'{txt}.py').is_file():
-        path = f"./system/plugins/{txt}.py"
-        await app.send_document(message.chat.id, document=path, caption=f"**PLUGINS** - __{txt}__\n\n@lightning_support_grup", force_document=True)
+    if  Path(f'system/plugins/{txt}.py').is_file():
+        path = f"system/plugins/{txt}.py"
+        try:
+            caption =  f"**PLUGINS** - __{txt}__\n**COMMAND** - {COMMAND_HELP[txt]}\n\n@lightning_support_grup"
+        except KeyError:
+            caption =  f"**PLUGINS** - __{txt}__\n\n@lightning_support_grup"
+        await app.send_document(message.chat.id, document=path, caption=caption, force_document=True)
     elif "sql" in message.text:
-        if not Path(f'{txt}.py').is_file():
+        if not Path(f'system/sqls/{txt}.py').is_file():
             await message.edit("**SQL DOES NOT EXSISTS!**")
         else:
-           path = f"./system/sqls/{txt}.py"
+           path = f"system/sqls/{txt}.py"
            await app.send_document(message.chat.id, document=path, caption=f"**SQL-FILE** - __{txt}__\n\n@lightning_support_grup", force_document=True)
     elif "requirements" in message.text:
         path = f"{txt}.txt"
         await app.send_document(message.chat.id, document=path, caption=f"**REQUIREMENTS-FILE** - __{txt}__\n\n@lightning_support_grup", force_document=True)
     else:
-        await message.edit("{} does not exsists! (make sure name is correct :/)".format(txt))
-    await message.delete()
+        await message.edit("**{} does not exsists! (make sure name it is correct :/)**".format(txt))
 
 
 COMMAND_HELP.update({
