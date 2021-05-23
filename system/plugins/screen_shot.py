2821ce8f7e185393e80ffe714717551564948e4c
Kei <73867910+KeinShin@users.noreply.github.com>
2021-05-23 20:33:23 +0530

    will see dis,

--- system/plugins/screen_shot.py
+++ system/plugins/screen_shot.py
@@ -1,15 +1,15 @@
-from system.plugins import light
+# from system.plugins import light
+# # from system import app
+# import pyautogui
+
+# import logging
+
 # from system import app
-import pyautogui
 
-import logging
-
-from system import app
-
-logging.basicConfig(level=logging.INFO)
-logging.basicConfig(level=logging.ERROR)
-@light.on(["ss", "screen shot"])
-async def screen_shot(client, message):
-  myScreenshot = pyautogui.screenshot()
-  myScreenshot.save(r'file name.png')
+# logging.basicConfig(level=logging.INFO)
+# logging.basicConfig(level=logging.ERROR)
+# @light.on(["ss", "screen shot"])
+# async def screen_shot(client, message):
+#   myScreenshot = pyautogui.screenshot()
+#   myScreenshot.save(r'file name.png')
 
