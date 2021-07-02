6410144a1ffe886381ddd2dfae40d317883280c1
Rishisuperyo OP <73427470+Rishisuperyo-Op@users.noreply.github.com>
2021-07-02 16:22:56 +0530

    Parmatin,Parmatin oo wah parmatin bl ka suspension hatwake mauj kardi wah wah

--- userbot/plugins/ophack.py
+++ userbot/plugins/ophack.py
@@ -2,7 +2,7 @@ from telethon import events
 import asyncio
 from userbot.utils import lightning_cmd
 from userbot import CMD_HELP
-@borg.on(events.NewMessage(pattern=r"ophack", outgoing=True))
+@borg.on(lightning_cmd(pattern=r"ophack", outgoing=True))
 async def hapy(event):
       a="Starting Op hack😎⚡ [/]"
       await event.edit(a)
