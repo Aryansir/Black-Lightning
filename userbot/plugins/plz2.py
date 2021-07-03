06dbbce7ce6c01fbc84639ddc44065d3088cbef2
Rishisuperyo OP <73427470+Rishisuperyo-Op@users.noreply.github.com>
2021-07-03 18:00:48 +0530

    Update plz2.py

--- userbot/plugins/plz2.py
+++ userbot/plugins/plz2.py
@@ -1,12 +1,12 @@
 #plugun by Rishisuperyo
 #kang = GEY
 #KEEP CREDITS OF RISHISUPERYO
-#usage .plzzz
+#usage .🥺plz
 from telethon import events
 import asyncio
 from userbot.utils import lightning_cmd
 from userbot import CMD_HELP
-@borg.on(lightning_cmd(pattern=r"plzzz", outgoing=True))
+@borg.on(lightning_cmd(pattern=r"🥺plz", outgoing=True))
 async def _(event):
      a="plz 🥺🙏🥺🙏🥺🙏🥺🙏🥺🙏🥺"
      await event.edit(a)
