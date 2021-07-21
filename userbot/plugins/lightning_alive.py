bc225326701a88e3fa96a115b0886c7852bf6222
Aditya <79218915+Paramatin-OP@users.noreply.github.com>
2021-07-21 20:08:42 +0530

    Update lightning_alive.py

--- userbot/plugins/lightning_alive.py
+++ userbot/plugins/lightning_alive.py
@@ -54,7 +54,7 @@ from userbot import CMD_LIST
 pm_caption = "**Black Lightning is online**\n\n"
 pm_caption += f"★ **Boss**            : {DEFAULTUSER}\n"
 pm_caption += "★ **Version**        : 1.17.5\n"
-pm_caption += "★ **Channel**𝙻         : [ᴊᴏɪɴ](https://t.me/lightning_support_channel)\n"
+pm_caption += "★ **Channel**          : [ᴊᴏɪɴ](https://t.me/lightning_support_channel)\n"
 
 pm_caption += "★ **Support Group**           : [ᴊᴏɪɴ](https://t.me/lightning_support_group)\n"
 
