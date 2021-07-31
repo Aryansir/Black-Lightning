148d5127563a81e8ec664621bcd97b4e10ae78e3
Aditya <79218915+Paramatin-OP@users.noreply.github.com>
2021-07-31 17:16:19 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -30,18 +30,21 @@ CMD_HNDLR = Config.CMD_HNDLR
 
 if Var.STRING_SESSION:
     session_name = str(Var.STRING_SESSION)
-    bot = TelegramClient(StringSession(session_name, Var.APP_ID, Var.API_HASH))
+    fak = Var.APP_ID
+    fek = Var.API_HASH
+    bot = TelegramClient(StringSession(session_name), fak, fek)
 else:
     session_name = "startup"
-    bot = TelegramClient(session_name, Var.APP_ID, Var.API_HASH)
-
+    fak = Var.APP_ID
+    fek = Var.API_HASH
+    bot = TelegramClient(session_name, fak, fek)
 
 
 INT_PLUG = ""
 LOAD_PLUG = {}
 COOL_CMD = {}
 # PaperPlaneExtended Support Vars
-ENV = os.environ.get("ENV", False)
+ENV = os.environ.get("ENV", "ANYTHING")
 """ PPE initialization. """
 
 
