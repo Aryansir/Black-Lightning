4180aeeef604a13030537b0192a41517d0cfb7dc
Aditya <79218915+Paramatin-OP@users.noreply.github.com>
2021-07-31 14:42:03 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -30,7 +30,7 @@ CMD_HNDLR = Config.CMD_HNDLR
 
 if Var.STRING_SESSION:
     session_name = str(Var.STRING_SESSION)
-    bot = TelegramClient(StringSession(session_name), Var.APP_ID, Var.API_HASH)
+    bot = TelegramClient(session_name, Var.APP_ID, Var.API_HASH)
 else:
     session_name = "startup"
     bot = TelegramClient(session_name, Var.APP_ID, Var.API_HASH)
